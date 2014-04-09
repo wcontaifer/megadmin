175a28ccefe514d0abe40eb52f1ea31d3e97ddb2
wanderson <wanderson@wanderson-linux-note>
2014-04-08 21:04:20 -0300

    Criação dos modulos contábil e imobiliária Criação dos controllers do contábil

---
+++ module/Contabil/src/Contabil/Controller/CadastroController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Contabil\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class CadastroController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function empresaAction()
+    {
+        return new ViewModel();
+    }
+
+    public function usuarioAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
